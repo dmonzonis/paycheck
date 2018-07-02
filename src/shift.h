892fcc89496070076fb4058e93fa7ebf3aeb9715
monzo94 <monzo_94@hotmail.com>
2018-07-03 01:27:40 +0200

    Document Shift class

--- src/shift.h
+++ src/shift.h
@@ -3,13 +3,44 @@
 
 #include <QTime>
 
+/**
+ * @brief The Shift class represents a range of working hours, from entry time
+ * to exit time.
+ *
+ * Entry and exit times are represented by QTime objects.
+ */
 class Shift
 {
 public:
+    /**
+     * @brief Create a new shift with the given entry and exit times.
+     */
     Shift(QTime entryTime, QTime exitTime);
+
+    /**
+     * @brief Return the total of hours in the range from entry to exit time.
+     */
     float getWorkingHours() const;
+
+    /**
+     * @brief Return the entry time.
+     */
     QTime getEntryTime() const;
+
+    /**
+     * @brief Return the exit time.
+     */
     QTime getExitTime() const;
+
+    /**
+     * @brief Return a string representing the shift.
+     *
+     * The format of the string is "h:mm - h:mm", starting with the entry time and
+     * ending with the exit time.
+     *
+     * For example, if entry time is 8:00 and exit time is 13:30, the returned string
+     * will be "8:00 - 13:30".
+     */
     std::string toString() const;
 
 private:
